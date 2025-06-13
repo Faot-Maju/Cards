45905ba15ae98987ebe3114fec3453606860b189
Faot-Maju <faot.maria@escola.pr.gov.br>
2025-06-13 17:32:34 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Zoologia',
+    'Qual o maior animal do planeta ? ',
+    'Baleia azul'
+)
+
+criaCartao(
+    'Geografia',
+    'Que país tem o formato de uma bota??',
+    'Itália'
+)
+
+criaCartao(
+    'História',
+    'Quem foi o primeiro líder da URSS?',
+    'Vladimir Lênin'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual animal marinho tem três corações e sangue azul?',
+    'O polvo'
+)
